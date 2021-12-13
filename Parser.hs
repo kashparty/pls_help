cd9715778cb68ec8720c44b79569e0b668b06916
Pratyaksh Sharma <sharma.pratyaksh@gmail.com>
2021-12-13 09:42:28 +0000

    Implement Token type and lexing Tokens

---
+++ Parser.hs
@@ -0,0 +1,25 @@
+data Token = Var Char 
+           | LBracket 
+           | RBracket 
+           | Not 
+           | And 
+           | Or 
+           | Implies 
+           | Iff
+           deriving (Show, Eq)
+
+-- Syntax example: ~p -> ~((~q | r) & s)
+getTokens :: String -> [Token]
+getTokens []
+  = []
+getTokens (x : xs)
+  = case x of
+      ' '  -> getTokens xs
+      '('  -> LBracket : getTokens xs
+      ')'  -> RBracket : getTokens xs
+      '~'  -> Not      : getTokens xs
+      '&'  -> And      : getTokens (tail xs)
+      '|' -> Or       : getTokens (tail xs)
+      '-'  -> Implies  : getTokens (tail xs)
+      '<'  -> Implies  : getTokens (tail $ tail xs)
+      v    -> Var v    : getTokens xs
