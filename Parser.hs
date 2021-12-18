f9824f43725fd692252060f50948a897534544fc
Pratyaksh Sharma <sharma.pratyaksh@gmail.com>
2021-12-18 17:35:19 +0000

    Implement token parsing

--- Parser.hs
+++ Parser.hs
@@ -45,3 +45,87 @@ getTokens (x : xs)
       '-' -> Implies  : getTokens (tail xs)
       '<' -> Iff      : getTokens (tail $ tail xs)
       v   -> Var v    : getTokens xs
+
+data ASTNode = ASTVar Char
+             | ASTNot ASTNode
+             | ASTAnd ASTNode ASTNode
+             | ASTOr ASTNode ASTNode
+             | ASTImplies ASTNode ASTNode
+             | ASTIff ASTNode ASTNode
+             deriving (Eq, Show)
+
+parseVar :: [Token] -> (ASTNode, [Token])
+parseVar (Var c : ts)
+  = (ASTVar c, ts)
+
+parsePrimary :: [Token] -> (ASTNode, [Token])
+parsePrimary (LBracket : ts)
+  = (node, rest)
+  where
+    (node, RBracket : rest) = parseIff ts
+parsePrimary ts
+  = parseVar ts
+
+parseNot :: [Token] -> (ASTNode, [Token])
+parseNot (Not : ts)
+  = (ASTNot node, rest)
+  where
+    (node, rest) = parsePrimary ts
+parseNot ts
+  = parsePrimary ts
+
+parseAnd :: [Token] -> (ASTNode, [Token])
+parseAnd ts
+  = parsePartialAnd left rest 
+  where
+    (left, rest) = parseNot ts
+
+    parsePartialAnd :: ASTNode -> [Token] -> (ASTNode, [Token])
+    parsePartialAnd left (And : rest)
+      = (ASTAnd left right, rest')
+      where
+        (right, rest') = parseNot rest
+    parsePartialAnd left rest
+      = (left, rest)
+
+parseOr :: [Token] -> (ASTNode, [Token])
+parseOr ts
+  = parsePartialOr left rest
+  where
+    (left, rest) = parseAnd ts
+
+    parsePartialOr :: ASTNode -> [Token] -> (ASTNode, [Token])
+    parsePartialOr left (Or : rest)
+      = (ASTOr left right, rest')
+      where
+        (right, rest') = parseAnd rest
+    parsePartialOr left rest
+      = (left, rest)
+
+parseImplies :: [Token] -> (ASTNode, [Token])
+parseImplies ts
+  = parsePartialImplies left rest
+  where
+    (left, rest) = parseOr ts
+
+    parsePartialImplies :: ASTNode -> [Token] -> (ASTNode, [Token])
+    parsePartialImplies left (Implies : rest)
+      = (ASTImplies left right, rest')
+      where
+        (right, rest') = parseOr rest
+    parsePartialImplies left rest
+      = (left, rest)
+
+parseIff :: [Token] -> (ASTNode, [Token])
+parseIff ts
+  = parsePartialIff left rest
+  where
+    (left, rest) = parseImplies ts
+
+    parsePartialIff :: ASTNode -> [Token] -> (ASTNode, [Token])
+    parsePartialIff left (Iff : rest)
+      = (ASTIff left right, rest')
+      where
+        (right, rest') = parseImplies rest
+    parsePartialIff left rest
+      = (left, rest)
